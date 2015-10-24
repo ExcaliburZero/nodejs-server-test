8eca20fe6d995fd00bb7d68a04b79c8c49c34c77
ExcaliburZero <cwellsny@nycap.rr.com>
2015-10-24 10:05:24 -0400

    Changed web server to be a static file server
    Commented out the code for the single file server, and added code to run a static file server.
    
    Also added a package file to list the dependencies needed by the application.

--- web_server.js
+++ web_server.js
@@ -1,3 +1,4 @@
+/* Display single page
 var http = require('http');
 
 // Create the web server
@@ -7,6 +8,17 @@ http.createServer(function (req, res) {
 	// Write the contents of the page
 	res.end('Hello World\n');
 }).listen(8080);
+*/
+
+// Run Java file
+child_process.exec("javac java/src/nodejsservertest/Main.java");
+
+/* Setup static file server */
+var express = require('express'),
+    app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(8080);
 
 // Print to the console that the server is running
 console.log('Server running on port 8080.');
